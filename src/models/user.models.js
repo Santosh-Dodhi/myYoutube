2169ce4b98ddd56bdeafce654ce26d03482e0e39
Santosh-Dodhi <santoshdodhi4@gmail.com>
2025-03-03 20:43:54 +0530

    Add the hash functionality in the user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,8 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const UserSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -16,7 +18,7 @@ const UserSchema = mongoose.Schema({
     unique: true,
     trim: true,
   },
-  fullname: {
+  fullName: {
     type: String,
     required: true,
     trim: true,
@@ -46,4 +48,20 @@ const UserSchema = mongoose.Schema({
 
 }, {timestamps: true})
 
-export const User = mongoose.model("User", UserSchema)
+userSchema.pre("save",  async function (next) { // before save the data this hook will be called 
+  //! We are using the function syntax instead  of arrow as we need to use the this context
+  // We also need to use the async - await as these operations could take time.
+  //? Also don't miss to add the next argument as it also needs.
+
+  if (!this.isModified("password")) return next(); // password not modified // here we need to mention the attribute name in String
+
+  this.password = await bcrypt.hash(this.password, 10)// here 10:: no. of hash rounds 
+  next();
+
+})
+
+userSchema.methods.isPassWordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password) //it directly returns boolean value `true` or `false`.
+}
+
+export const User = mongoose.model("User", userSchema)
