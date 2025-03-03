03c69183660a86c7246c696e537256f39a896a57
Santosh-Dodhi <santoshdodhi4@gmail.com>
2025-03-03 21:02:41 +0530

    Add the jwt token in the user model.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -2,66 +2,95 @@ import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true,
-    index: true,
-    trim: true,
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      index: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary url
+      index: true,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "This field is required."],
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true,
-    trim: true,
-  },
-  fullName: {
-    type: String,
-    required: true,
-    trim: true,
-    index: true
-  },
-  avatar: {
-    type: String, //cloudinary url
-    required: true
-  },
-  coverImage: {
-    type: String, //cloudinary url 
-    index: true
-  },
-  watchHistory: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Video"
-    }
-  ],
-  password: {
-    type: String,
-    required: [true, "This field is required."]
-  },
-  refreshToken: {
-    type: String
-  }
+  { timestamps: true }
+);
 
-}, {timestamps: true})
-
-userSchema.pre("save",  async function (next) { // before save the data this hook will be called 
+userSchema.pre("save", async function (next) {
+  // before save the data this hook will be called
   //! We are using the function syntax instead  of arrow as we need to use the this context
   // We also need to use the async - await as these operations could take time.
   //? Also don't miss to add the next argument as it also needs.
 
   if (!this.isModified("password")) return next(); // password not modified // here we need to mention the attribute name in String
 
-  this.password = await bcrypt.hash(this.password, 10)// here 10:: no. of hash rounds 
+  this.password = await bcrypt.hash(this.password, 10); // here 10:: no. of hash rounds
   next();
-
-})
+});
 
 userSchema.methods.isPassWordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password) //it directly returns boolean value `true` or `false`.
+  return await bcrypt.compare(password, this.password); //it directly returns boolean value `true` or `false`.
+};
+
+userSchema.models.generateAccessToken = async function () {
+  return await jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.refreshAccessToken = async function () {
+  return await jwt.signJ(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  )
 }
 
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model("User", userSchema);
